b3e19f238ec45c498cbce53e5746a31d998fbdb2
Moaz Dawalibi <moaz.dawalibi@sdnone.net>
2024-02-06 16:50:48 +0300

    .

--- src/api/helper/useAxios.ts
+++ src/api/helper/useAxios.ts
@@ -1,20 +1,28 @@
 import { BaseURL } from '../config'
 import useAuthState from '../../lib/state mangment/AuthState'
 import AxiosBuilder from './AxiosBuilder'
+import { useTranslation } from 'react-i18next'
 
 function useAxios() {
   const {isAuthenticated , token}= useAuthState()  
-    
+  const {i18n} = useTranslation()
+  console.log(i18n.language);
+  
   const buildAxios = new AxiosBuilder().
   withBaseURL(BaseURL)
   .withResponseType('json')
   .withTimeout(120000)
-  .withHeaders({"Content-Type" :"application/json"})
+  .withHeaders(
+    {"Content-Type" :"application/json",
+  "language":i18n.language
+})
 
   
   if(isAuthenticated){
 
-    buildAxios.withHeaders({ Authorization: 'Bearer '+ token })
+    buildAxios.withHeaders({ Authorization: 'Bearer '+ token ,
+    "Content-Type" :"application/json",
+    "language":i18n.language })
   } 
   
   return (
