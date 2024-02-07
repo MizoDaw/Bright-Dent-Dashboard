84d2488404d3f0884e222e2d824576df0024b6ea
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2024-02-07 10:39:04 +0300

    update editor

--- src/Extensions/Editor/HtmlEditor.tsx
+++ src/Extensions/Editor/HtmlEditor.tsx
@@ -1,6 +1,26 @@
-import React, { FC } from "react";
-import { Editor } from '@tinymce/tinymce-react';
+import React, { FC, useState } from "react";
+// import { Editor } from '@tinymce/tinymce-react';
 import { useFormikContext } from "formik";
+import { 
+  BtnBold,
+  BtnItalic,
+  Editor,
+  EditorProvider,
+  BtnStyles,
+  BtnLink,
+  BtnRedo,
+  BtnStrikeThrough,
+  BtnBulletList,
+  BtnClearFormatting,
+  BtnUnderline,
+  BtnNumberedList,
+  BtnUndo,
+  Toolbar,
+  
+} from 'react-simple-wysiwyg';
+
+
+
 
 interface HtmlEditorProps {
   langCode: number;
@@ -10,32 +30,37 @@ interface HtmlEditorProps {
 
 const HtmlEditor: FC<HtmlEditorProps> = ({ langCode, name, editorState, ...props }) => {
   const formik = useFormikContext();
-  
-  const ar: boolean = langCode === 2;
+  const [value, setValue] = useState(formik.getFieldProps(name).value);
+
+  function onChange(e:any) {
+    console.log(e.target.value);
+    formik.setFieldValue(name , e.target.value);
+    setValue(e.target.value);
+  }
 
   return (
-    <Editor
-      apiKey='6xf0byrgd7m2j28p9dfjittsq884x9j0d3e6dsterqrvtvez'
-      value={editorState}
-      init={{
-        height: 500,
-        menubar: false,
-        directionality: ar ? "rtl" : "ltr",
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount'
-        ],
-        toolbar: 'undo redo | formatselect | ' +
-          'bold italic backcolor | alignleft aligncenter ' +
-          'alignright alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | help',
-        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-      }}
-      onEditorChange={(newValue, editor) => {
-        formik.setFieldValue(name, newValue)
-      }}
-    />
+    <EditorProvider>
+
+    <Editor value={value} onChange={onChange}  dir={langCode == 2 ?"rtl":'ltr'} >
+        <Toolbar>
+          <BtnBold />
+          <BtnItalic />
+          <BtnUndo/>
+          <BtnNumberedList/>
+          <BtnUnderline/>
+          <BtnClearFormatting/>
+          <BtnBulletList/>
+          <BtnStrikeThrough/>
+          <BtnLink/>
+          <BtnStyles/>
+          <BtnRedo/>
+     
+
+        </Toolbar>
+      </Editor>
+    </EditorProvider>
+    
+    
   );
 }
 
