495d29345422d55d522bdedd32e313ba6e860285
Moaz Dawalibi <moaz.dawalibi@sdnone.net>
2024-02-11 13:06:55 +0300

    change url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
     
-// export const BaseURL = `https://apident.qtrendqa.com/`;
+export const BaseURL = `https://apident.qtrendqa.com/`;
 
-export const BaseURL = `http://192.168.1.15:8000/`;
+// export const BaseURL = `http://192.168.1.15:8000/`;
 
 
 export const BaseURL_IMAGE = BaseURL.slice(0,-1);
